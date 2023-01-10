96a8faf34091573a450e3a96bcf01c400009eeb3
Luka <licina.luka@outlook.com>
2023-01-10 18:40:38 +0100

    csvs

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,7 +1,40 @@
 (ns fwpd.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(def filename "suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int [s]
+  (Integer. s))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  [s]
+  (map (fn [e] (str/split e #","))
+       (str/split s #"\n")))
+
+(defn mapify [rows]
+  (map
+   (fn [unmapped]
+     (reduce
+      (fn [row-map [vamp-key value]]
+        (assoc row-map vamp-key (convert vamp-key value)))
+      {}
+      (map vector vamp-keys unmapped)))
+   rows))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (->> filename
+       slurp
+       parse
+       mapify
+       prn))
